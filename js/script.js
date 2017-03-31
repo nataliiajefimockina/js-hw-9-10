95bf3a05e1b6be776cd1a548114ac4efb5ce765d
Nataliia Jefimockina <natik.rusakova@gmail.com>
2017-03-31 20:15:28 +0300

    A carousel has created.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function() {
+
+    //jCarousel
+    $('.jcarousel').jcarousel({
+        wrap: 'circular',
+    });
+
+    $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+
+    $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }
+            });
+
+    // select
+    $('select').uniform();
+});
